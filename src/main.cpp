cd865cd14a1d1cab8a531377fdcb09700d8733ba
David Dawes <virtualsoundnw+github@gmail.com>
2018-08-12 23:30:07 -0700

    Get dev down to master (#111)
    * Adding Brewfile for MacOS build dependencies
    
    * Updating version, annotating Mac fetch-params.sh for debugging
    
    * updating from master (#91)
    
    * Asher dev (#89)
    
    * Adding Brewfile for fetching MacOS build dependencies with the brew bundle command.
    
    * Updating version, annotating Mac fetch-params.sh for debugging
    
    * Modify version to 0.3.10. (#90)
    
    * Debugging Windows komodod crash on legacy hardware that does not support AES or AVX instructions.
    Removing deploy steps in Travis-CI to use as a sanity check on Github
    
    * troubleshooting komodod on legacy hardware running Windows
    
    * debugging
    
    * Correcting syntax errors on debug
    saving progress on verus-cli windows InstallBuilder configuration xml
    
    * Fail on get_chainactive before lock if checks fail
    
    * Fix check for stake transaction after Komodo merge
    
    * Portable dev (#105)
    
    * Force portable code
    
    * Switch to old MMX instructions, avoiding SSE & SSE2 instructions.
    
    * Less agressive, leave verus code (which checks for it) using advanced instructions.
    
    * Compiling only for windows
    
    * Update .gitlab-ci.yml
    
    * Try -march=native for C++ code generation.
    
    * Tweaking machine architecture settings.
    
    * Try native alone.
    
    * Also get LIBCRYPTOCONDITIONS to -march=native
    
    * Switch other lib to native as well.
    
    * Try switching back further for CPU architecture.
    
    * Even lower end settings.
    
    * Turn on symbols.
    
    * Use sse2,3 and 4 capable x64.
    
    * Once again let verus lib use advances instructioins since it checjs via CPUID at run time.
    
    * Modify a few more makefile entries.
    
    * Switch to AMD model similar to our test system.
    
    * Get snark makefile to k8 too.
    
    * Yet another -march to modify to k8, or two of them.
    
    * Brute force k8 settings, comment non-portable code out.
    
    * Put the condition on cpuid back.
    
    * Put non-portable advenced instruction code back
    
    * Enable instructions.
    
    * Add lib for separate settings.
    
    * Update .gitlab-ci.yml
    
    * Update .gitlab-ci.yml
    
    * Update .gitlab-ci.yml
    
    * Update .gitlab-ci.yml
    
    * replacing k8-sse3 specific flags to x86-84
    
    * updating versions
    
    * updating versions
    
    * Get verus-cli verusd updated (#106)
    
    * Force portable code
    
    * Switch to old MMX instructions, avoiding SSE & SSE2 instructions.
    
    * Less agressive, leave verus code (which checks for it) using advanced instructions.
    
    * Compiling only for windows
    
    * Update .gitlab-ci.yml
    
    * Try -march=native for C++ code generation.
    
    * Tweaking machine architecture settings.
    
    * Try native alone.
    
    * Also get LIBCRYPTOCONDITIONS to -march=native
    
    * Switch other lib to native as well.
    
    * Try switching back further for CPU architecture.
    
    * Even lower end settings.
    
    * Turn on symbols.
    
    * Use sse2,3 and 4 capable x64.
    
    * Once again let verus lib use advances instructioins since it checjs via CPUID at run time.
    
    * Modify a few more makefile entries.
    
    * Switch to AMD model similar to our test system.
    
    * Get snark makefile to k8 too.
    
    * Yet another -march to modify to k8, or two of them.
    
    * Brute force k8 settings, comment non-portable code out.
    
    * Put the condition on cpuid back.
    
    * Put non-portable advenced instruction code back
    
    * Enable instructions.
    
    * Add lib for separate settings.
    
    * Update .gitlab-ci.yml
    
    * Update .gitlab-ci.yml
    
    * Update .gitlab-ci.yml
    
    * Update .gitlab-ci.yml
    
    * replacing k8-sse3 specific flags to x86-84
    
    * updating versions
    
    * Propagate verusd changes.
    
    * Revert "Merge branch 'Asher-Dev' into dev"
    
    This reverts commit 825f31148362141d13fe990bf509c49b5ec07161, reversing
    changes made to ecbd54c3b46149b957bb95f2a619e49f01dd10f5.
    
    * removing genproclimit=0 parameter from verusd scripts
    
    * removing gen parameter from verusd scripts

--- src/main.cpp
+++ src/main.cpp
@@ -3171,7 +3171,7 @@ bool static DisconnectTip(CValidationState &state, bool fBare = false) {
     for (int i = 0; i < block.vtx.size(); i++)
     {
         CTransaction &tx = block.vtx[i];
-        if ((i == (block.vtx.size() - 1) && block.IsVerusPOSBlock()))
+        if ((i == (block.vtx.size() - 1)) && (block.IsVerusPOSBlock() || (komodo_isPoS((CBlock *)&block) != 0)))
         {
             EraseFromWallets(tx.GetHash());
         }
